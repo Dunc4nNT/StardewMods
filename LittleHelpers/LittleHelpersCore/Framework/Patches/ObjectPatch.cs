dd8139ac5db2a667adc1d77d7a6ac8bcc50d3462
Dunc4nNT <76918336+Dunc4nNT@users.noreply.github.com>
2024-03-17 22:05:05 +0100

    temp

--- LittleHelpers/LittleHelpersCore/Framework/Patches/ObjectPatch.cs
+++ LittleHelpers/LittleHelpersCore/Framework/Patches/ObjectPatch.cs
@@ -205,31 +205,28 @@ namespace NeverToxic.StardewMods.LittleHelpersCore.Framework.Patches
         {
             try
             {
-                if (__instance.QualifiedItemId.StartsWith($"(BC){s_assetsModId}"))
+                if (__instance.QualifiedItemId.StartsWith($"(BC){s_assetsModId}") && DataLoader.BigCraftables(Game1.content).TryGetValue(__instance.ItemId, out BigCraftableData bigCraftableData))
                 {
                     // TODO: execute assigned actions
                     s_monitor.Log(__instance.QualifiedItemId);
-                    if (DataLoader.BigCraftables(Game1.content).TryGetValue(__instance.ItemId, out BigCraftableData bigCraftableData))
-                    {
-                        if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.HarvestCrops", out string doHarvestCrops) && bool.TryParse(doHarvestCrops, out bool doHarvestCropsBool) && doHarvestCropsBool)
-                            s_monitor.Log("Harvested crops.");
-                        if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.HarvestTrees", out string doHarvestTrees) && bool.TryParse(doHarvestTrees, out bool doHarvestTreesBool) && doHarvestTreesBool)
-                            s_monitor.Log("Harvested trees.");
-                        if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.WaterCrops", out string doWaterCrops) && bool.TryParse(doWaterCrops, out bool doWaterCropsBool) && doWaterCropsBool)
-                            s_monitor.Log("Watered crops.");
-                        if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.ReplantCrops", out string doReplantCrops) && bool.TryParse(doReplantCrops, out bool doReplantCropsBool) && doReplantCropsBool)
-                            s_monitor.Log("Replanted crops.");
-                        if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.GatherForagables", out string doGatherForagables) && bool.TryParse(doGatherForagables, out bool doGatherForagablesBool) && doGatherForagablesBool)
-                            s_monitor.Log("Gathered foragables.");
-                        if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.GatherTruffles", out string doGatherTruffles) && bool.TryParse(doGatherTruffles, out bool doGatherTrufflesBool) && doGatherTrufflesBool)
-                            s_monitor.Log("Gathered truffles.");
-                        if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.DigArtefacts", out string doDigArtefacts) && bool.TryParse(doDigArtefacts, out bool doDigArtefactsBool) && doDigArtefactsBool)
-                            s_monitor.Log("Dug artefacts.");
-                        if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.HarvestCrabPots", out string doHarvestCrabPots) && bool.TryParse(doHarvestCrabPots, out bool doHarvestCrabPotsBool) && doHarvestCrabPotsBool)
-                            s_monitor.Log("Harvested crab pots.");
-                        if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.AddBaitToCrabPots", out string doAddBaitToCrabPots) && bool.TryParse(doAddBaitToCrabPots, out bool doAddBaitToCrabPotsBool) && doAddBaitToCrabPotsBool)
-                            s_monitor.Log("Added bait to crab pots.");
-                    }
+                    if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.HarvestCrops", out string doHarvestCrops) && bool.TryParse(doHarvestCrops, out bool doHarvestCropsBool) && doHarvestCropsBool)
+                        s_monitor.Log("Harvested crops.");
+                    if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.HarvestTrees", out string doHarvestTrees) && bool.TryParse(doHarvestTrees, out bool doHarvestTreesBool) && doHarvestTreesBool)
+                        s_monitor.Log("Harvested trees.");
+                    if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.WaterCrops", out string doWaterCrops) && bool.TryParse(doWaterCrops, out bool doWaterCropsBool) && doWaterCropsBool)
+                        s_monitor.Log("Watered crops.");
+                    if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.ReplantCrops", out string doReplantCrops) && bool.TryParse(doReplantCrops, out bool doReplantCropsBool) && doReplantCropsBool)
+                        s_monitor.Log("Replanted crops.");
+                    if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.GatherForagables", out string doGatherForagables) && bool.TryParse(doGatherForagables, out bool doGatherForagablesBool) && doGatherForagablesBool)
+                        s_monitor.Log("Gathered foragables.");
+                    if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.GatherTruffles", out string doGatherTruffles) && bool.TryParse(doGatherTruffles, out bool doGatherTrufflesBool) && doGatherTrufflesBool)
+                        s_monitor.Log("Gathered truffles.");
+                    if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.DigArtefacts", out string doDigArtefacts) && bool.TryParse(doDigArtefacts, out bool doDigArtefactsBool) && doDigArtefactsBool)
+                        s_monitor.Log("Dug artefacts.");
+                    if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.HarvestCrabPots", out string doHarvestCrabPots) && bool.TryParse(doHarvestCrabPots, out bool doHarvestCrabPotsBool) && doHarvestCrabPotsBool)
+                        s_monitor.Log("Harvested crab pots.");
+                    if (bigCraftableData.CustomFields.TryGetValue($"{s_assetsModId}.AddBaitToCrabPots", out string doAddBaitToCrabPots) && bool.TryParse(doAddBaitToCrabPots, out bool doAddBaitToCrabPotsBool) && doAddBaitToCrabPotsBool)
+                        s_monitor.Log("Added bait to crab pots.");
                 }
             }
             catch (Exception e)
