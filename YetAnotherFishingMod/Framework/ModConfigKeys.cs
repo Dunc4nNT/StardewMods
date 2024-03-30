f7177c2221d8ae968be3a558aa806f0629ccf91f
Dunc4nNT <76918336+Dunc4nNT@users.noreply.github.com>
2024-03-30 20:10:11 +0100

    move modconfigkeys into the same file as modconfig

--- YetAnotherFishingMod/Framework/ModConfigKeys.cs
+++
@@ -1,9 +0,0 @@
-﻿using StardewModdingAPI.Utilities;
-
-namespace NeverToxic.StardewMods.YetAnotherFishingMod.Framework
-{
-    internal class ModConfigKeys
-    {
-        public KeybindList ReloadConfig { get; set; } = new();
-    }
-}
