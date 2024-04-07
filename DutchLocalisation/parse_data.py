3f600b4d0c4b5f4039270be18d1eb23baa6d327d
Dunc4nNT <76918336+Dunc4nNT@users.noreply.github.com>
2024-04-07 18:34:12 +0200

    temp

--- DutchLocalisation/parse_data.py
+++ DutchLocalisation/parse_data.py
@@ -10,54 +10,66 @@ def parse() -> None:
     current_date = datetime.datetime.now(datetime.UTC)
     folderdate = f"{current_date.year}-{current_date.month}-{current_date.day}"
 
-    Path(f"./data/output-{folderdate}/assets/vanilla/").mkdir(parents=True, exist_ok=True)
+    Path(f"./data/output-{folderdate}/assets/vanilla/").mkdir(
+        parents=True, exist_ok=True
+    )
     outputpath_vanilla = Path(f"./data/output-{folderdate}/assets/vanilla/")
 
     for file in german_data:
-        gamefilepath = str(file.parent).replace("data\\input\\vanilla\\", "").replace("\\", "/")
+        gamefilepath = (
+            str(file.parent).replace("data\\input\\vanilla\\", "").replace("\\", "/")
+        )
 
         if gamefilepath == "Fonts":
             continue
 
-        _ = Path(f"{outputpath_vanilla}/{gamefilepath}").mkdir(parents=True, exist_ok=True),
+        _ = (
+            Path(f"{outputpath_vanilla}/{gamefilepath}").mkdir(
+                parents=True, exist_ok=True
+            ),
+        )
         shutil.copy(
-            Path(str(file).replace(".de-DE", "")),
-            Path(f"{outputpath_vanilla}/{gamefilepath}/{file.name.replace(".de-DE", ".nl-NL")}"),
+            Path(str(file)),
+            Path(f"{outputpath_vanilla}/{gamefilepath}/{file.name}"),
         )
 
-    outputpath = Path(f"./data/output-{folderdate}/assets/")
-    outputpath_folders = list(outputpath.glob("**/"))
-    
-    for path in outputpath_folders:
-        Path(f"{path}/__content.json").touch()
+    # outputpath = Path(f"./data/output-{folderdate}/assets/")
+    # outputpath_folders = list(outputpath.glob("**/"))
 
-    for path in outputpath_folders:
-        content_file = Path(f"{path}/__content.json")
-        with open(content_file, "w") as fw:
-            fw.writelines([
-                "{\n",
-                '  "$schema": "https://smapi.io/schemas/content-patcher.json",\n',
-                '  "Changes": [\n'
-            ])
+    # for path in outputpath_folders:
+    #     Path(f"{path}/__content.json").touch()
 
-            jsonfiles: list[Path] = []
-            for sub in path.iterdir():
-                if sub.is_dir():
-                    jsonfiles += (list(sub.glob("__content.json")))
-            jsonfiles += list(path.glob("*.json"))
+    # for path in outputpath_folders:
+    #     content_file = Path(f"{path}/__content.json")
+    #     with open(content_file, "w") as fw:
+    #         fw.writelines(
+    #             [
+    #                 "{\n",
+    #                 '  "$schema": "https://smapi.io/schemas/content-patcher.json",\n',
+    #                 '  "Changes": [\n',
+    #             ]
+    #         )
 
-            for file in jsonfiles:
-                if file == content_file:
-                    continue
+    #         jsonfiles: list[Path] = []
+    #         for sub in path.iterdir():
+    #             if sub.is_dir():
+    #                 jsonfiles += list(sub.glob("__content.json"))
+    #         jsonfiles += list(path.glob("*.json"))
 
-                fw.writelines([
-                    "    {\n",
-                    '      "Action": "Include",\n',
-                    f'      "FromFile": "assets{str(Path(str(file).replace(str(outputpath), ""))).replace("\\", "/")}"\n',
-                    "    },\n"
-                ])
-            
-            fw.writelines(["  ]\n", "}\n"])
+    #         for file in jsonfiles:
+    #             if file == content_file:
+    #                 continue
+
+    #             fw.writelines(
+    #                 [
+    #                     "    {\n",
+    #                     '      "Action": "Include",\n',
+    #                     f'      "FromFile": "assets{str(Path(str(file).replace(str(outputpath), ""))).replace("\\", "/")}"\n',
+    #                     "    },\n",
+    #                 ]
+    #             )
+
+    #         fw.writelines(["  ]\n", "}\n"])
 
 
 if __name__ == "__main__":
