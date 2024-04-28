b6ed59c631e1adde0a775e202aa1827b219f4dc7
Dunc4nNT <76918336+Dunc4nNT@users.noreply.github.com>
2024-04-28 07:25:15 +0200

    project template

---
+++ SmapiTemplate/Framework/GenericModConfigMenu.cs
@@ -0,0 +1,19 @@
+﻿using NeverToxic.StardewMods.Common;
+using StardewModdingAPI;
+using System;
+
+namespace NeverToxic.StardewMods.SmapiTemplate.Framework
+{
+    internal class GenericModConfigMenu(IModRegistry modRegistry, IManifest manifest, IMonitor monitor, Func<ModConfig> config, Action reset, Action save)
+    {
+        public void Register()
+        {
+            IGenericModConfigMenuApi configMenu = modRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+
+            if (configMenu is null)
+                return;
+
+            configMenu.Register(mod: manifest, reset: reset, save: save);
+        }
+    }
+}
---
+++ TelevisionFramework/Framework/Patches.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using StardewModdingAPI;
+using System;
+
+namespace NeverToxic.StardewMods.TelevisionFramework.Framework
+{
+    internal class Patches
+    {
+        private static Harmony s_harmony;
+        private static IMonitor s_monitor;
+        private static Func<ModConfig> s_config;
+        private static IReflectionHelper s_reflectionHelper;
+
+        internal static void Initialise(Harmony harmony, IMonitor monitor, Func<ModConfig> config, IReflectionHelper reflectionHelper)
+        {
+            s_harmony = harmony;
+            s_monitor = monitor;
+            s_config = config;
+            s_reflectionHelper = reflectionHelper;
+
+            ApplyPatches();
+        }
+
+
+        private static void ApplyPatches()
+        {
+        }
+    }
+}
